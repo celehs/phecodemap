00141ae5534d3009e0b7f464932353e30fc0c150
chenh2017 <404746965@qq.com>
2024-01-31 14:39:07 +0000

    change app_title

--- R/app_ui.R
+++ R/app_ui.R
@@ -129,7 +129,7 @@ golem_add_external_resources <- function(){
     favicon(ext = 'png'),
     bundle_resources(
       path = app_sys('app/www'),
-      app_title = 'phecodemap'
+      app_title = 'PheCode to ICD map'
     )
     # Add here other external resources
     # for example, you can add shinyalert::useShinyalert() 
